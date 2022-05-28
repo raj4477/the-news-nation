d78360e4c80b7438abf499515c33122951dd7674
Ritik Raj <80347492+raj4477@users.noreply.github.com>
2022-05-28 14:19:58 +0530

    Made Some Changes

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
-const my_api_key = "0b5e9e50c60048a19d8f05e7f5d2d93c";
+// const my_api_key = "0b5e9e50c60048a19d8f05e7f5d2d93c";
+const my_api_key = "71607bc79d91474da3b3572648d605ac";
 
 module.exports = my_api_key ;
