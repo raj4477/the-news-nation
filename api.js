4ca0c08a2da91d9586b31ea348aefaf8da4278f2
Ritik Raj <80347492+raj4477@users.noreply.github.com>
2023-03-02 23:56:09 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
 // const my_api_key = "0b5e9e50c60048a19d8f05e7f5d2d93c";
-const my_api_key = "71607bc79d91474da3b3572648d605ac";
+const my_api_key = "0b5e9e50c60048a19d8f05e7f5d2d93c";
 
 module.exports = my_api_key ;
